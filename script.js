8edd62f523ae2ad3b9e0b79db070a1d51c4e49dd
said <soidroudinesaid@hotmail.fr>
2024-04-02 12:06:38 +0200

    Modification de la section avis

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.getElementById('menu-btn');
-   
     const navbarList = document.querySelector('.mobile-first');
-    
     const closeBtn = document.getElementById('close-btn');
-
     const cartBtn = document.getElementById('cart-icon');
-   
     const panierProduct = document.querySelector('.cart');
-
     const closeBtnCart = document.getElementById('close-btn-cart');
 
     menuBtn.addEventListener('click', function() {
@@ -31,9 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 /* Témoignages des clients dynamique*/
-
 const slider2 = document.querySelector('.slider2');
 const slides2 = document.querySelectorAll('.slide2');
 const slide2Width = slides2[0].offsetWidth;
@@ -57,9 +48,7 @@ function updateSlider2() {
     });
 }
 
-
 const slider2Dots = document.querySelector('.slider2-dots');
-
 for (let i = 0; i < slides2.length; i++){
     const dot = document.createElement('div');
     dot.classList.add('dot');
@@ -69,7 +58,5 @@ for (let i = 0; i < slides2.length; i++){
     });
     slider2Dots.appendChild(dot);
 }
-
-
 setInterval(nextSlide2, 1800);
 
